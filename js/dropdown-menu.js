9b45cd827f67970073eb87ef9bf3f055a11bea5d
maruf-i <maruf@cubicmelon.com>
2015-07-04 02:18:10 +0100

    3 page completed
    Way to go

---
+++ js/dropdown-menu.js
@@ -0,0 +1,15 @@
+//dropdown menu function for all pages, connected with the template file.
+
+/*var menu = function (){
+  $('dropdown-toggle').click(function(){
+      $('dropdown-menu').toggle();
+
+  });
+
+}
+
+$(document).ready(manu);*/
+
+$(function(){
+      $(".dropdown-toggle").dropdown('toggle');
+      });
